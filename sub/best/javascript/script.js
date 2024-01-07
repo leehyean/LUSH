109475be50dbf8f3cded4b10a8f032974837f23f
leehyean <93965591+leehyean@users.noreply.github.com>
2024-01-07 21:00:21 +0900

    신제품, 베스트 페이지
    서브 페이지(신제품, 베스트) 페이지 반응형 페이지

--- sub/best/javascript/script.js
+++ sub/best/javascript/script.js
@@ -1,16 +1,16 @@
 
 AOS.init();
 
-var swiper = new Swiper(".section_2 .mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 0,
-  slidesPerGroup: 4,
-//   loop: true,
-//   autoplay: true,
-//   autoplay: {
-//     delay: 4000,
+var swiper = new Swiper(".section_1 .mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: true,
+  autoplay: true,
+  autoplay: {
+    delay: 4000,
 
-//   },
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -20,107 +20,37 @@ var swiper = new Swiper(".section_2 .mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 
-  // breakpoints: { 반응형 width 
 
-  //   1020: { 640 이상일 경우
-
-  //   slidesPerView: 1,  }, 레이아웃 2 },
-
-  //   768: { 
-  //     slidesPerView: 3, 
-  //   },
-
-  //   },
-
-  breakpoints: { 
-
-    // 1020: { 640 이상일 경우
-
-    // slidesPerView: 1,  }, 레이아웃 2 },
-
+  // breakpoints: { 
   
 
-    1280: { 
-      slidesPerView: 4, 
-      slidesPerGroup: 4,
-    },
+  //   1280: { 
+  //     slidesPerView: 4, 
+  //     slidesPerGroup: 4,
+  //   },
 
-    510: { 
-      slidesPerView: 2, 
-      slidesPerGroup: 2,
-    },
+  //   510: { 
+  //     slidesPerView: 2, 
+  //     slidesPerGroup: 2,
+  //   },
 
-    0: { 
-      slidesPerView: 1, 
-      slidesPerGroup: 1,
-    },
+  //   0: { 
+  //     slidesPerView: 1, 
+  //     slidesPerGroup: 1,
+  //   },
 
-    },
+  //   },
 
     
 
 
 });
 
-var swiper = new Swiper(".section_4 .mySwiper", {
-  slidesPerView: 2,
-  spaceBetween: 0,
-  slidesPerGroup: 2,
-//   loop: true,
-//   autoplay: true,
-//   autoplay: {
-//     delay: 4000,
-
-//   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // breakpoints: { 반응형 width 
-
-  //   1020: { 640 이상일 경우
-
-  //   slidesPerView: 1,  }, 레이아웃 2 },
-
-  //   768: { 
-  //     slidesPerView: 3, 
-  //   },
-
-  //   },
-
-  breakpoints: { 
-
-    // 1020: { 640 이상일 경우
-
-    // slidesPerView: 1,  }, 레이아웃 2 },
-
-  
-
-    1280: { 
-      slidesPerView: 2, 
-      slidesPerGroup: 2,
-    },
-
-    790: { 
-      slidesPerView: 2, 
-      slidesPerGroup: 2,
-    },
-
-    0: { 
-      slidesPerView: 1, 
-      slidesPerGroup: 1,
-    },
-
-    },
+// section_1
 
     
 
 
-});
+
 
 
